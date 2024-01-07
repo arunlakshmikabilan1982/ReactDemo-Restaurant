bd8560926877869beba0b05ea4120a7e77919d1a
pkrishnavamsi0709 <pkrishnavamsi2001@gmail.com>
2024-01-07 23:33:00 +0530

    NavBar
    added Navbar to the page

--- src/App.js
+++ src/App.js
@@ -5,20 +5,60 @@ import { Menu } from "./components/Menu";
 import { Scroll } from "./components/Scroll";
 import { ContactUs } from "./components/ContactUs";
 import Specials from "./components/Specials";
+import NavBar from "./NavBar";
+import { Whyus } from "./components/Whyus";
+
+const NavBarItems = [
+  {
+    name: "Home",
+    href: "https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_select",
+  },
+  {
+    name: "Gallery",
+    href: "https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_select",
+  },
+  {
+    name: "Home",
+    href: "https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_select",
+  },
+  {
+    name: "Chefs",
+    href: "https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_select",
+  },
+  {
+    name: "Orders",
+    href: "https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_select",
+  },
+
+  {
+    name: "Events",
+    href: "https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_select",
+  },
+  {
+    name: "Specials",
+    href: "https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_select",
+  },
+  {
+    name: "Menu",
+    href: "https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_select",
+  },
+  {
+    name: "about",
+    href: "https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_select",
+  },
+];
 
 function App() {
   return (
     <div className="App">
+      <NavBar title="Resturant" items={NavBarItems} />
       <Menu />
       <Main />
       {/* <div className="ScollerBg">
         <Scroll />
   </div> */}
-
-      <ContactUs />
-      <div></div>
-
       <Specials />
+      <ContactUs />
     </div>
   );
 }
--- src/components/Specials.js
+++ src/components/Specials.js
@@ -1,5 +1,6 @@
 import { Heading } from "./Heading";
 import SpecialsMain from "./SpecialsMain";
+import { Underline } from "./Underline";
 import "./components.css";
 
 function Specials() {
@@ -7,6 +8,7 @@ function Specials() {
     <div className="MainBox">
       <Heading title="SPECAILS" description="Check Out Tasty Menu" />
       <SpecialsMain />
+      <Underline />
     </div>
   );
 }
